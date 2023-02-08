83ecd6662036a8328ac2d71ceb321e2fa7523e47
carreraprogrammer <109081622+carreraprogrammer@users.noreply.github.com>
2023-02-08 17:31:36 -0500

    Add depaginators

--- movie-facts.rb
+++ movie-facts.rb
@@ -22,3 +22,39 @@ module MovieFacts
       end
     end
   end
+
+  # DRIVER 
+
+  module MovieFacts
+    class HttpDriver
+      def fetch_data
+        # fetch data over HTTP
+        # cache results
+      end
+    end
+  end
+
+  module MovieFacts
+    class CacheDriver
+      def fetch_data
+        # read data from cache
+      end
+    end
+  end
+
+# DEPAGINATOR
+
+module MovieFacts
+    class Depaginator
+      def depaginate(data)
+        # depaginate the data
+      end
+    end
+  end
+  module MovieFacts
+    class NoopDepaginator
+      def depaginate(data)
+        data # just return the data without de-paginating it
+      end
+    end
+  end
